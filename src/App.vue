b2e48bdfaf3d4133bffeef3b5f749770d7ec3b79
sisdream <sissipascale2@gmail.com>
2024-04-12 19:52:30 +0200

    feat: creato scss

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
-<script setup>
-import AppTitle from './components/AppTitle.vue';
-import AppHero from './components/AppHero.vue';
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      title: "Hello world!",
+      tech_badges: [],
+    };
+  },
+
+  methods: {
+    fecthProjects() {
+      axios.get(`http://127.0.0.1:8000/api/projects`).then((res) => {
+        console.log(res.data.data);
+        this.tech_badges = res.data.data[0].technologies_badges;
+      });
+    },
+  },
+
+  mounted() {
+    this.fecthProjects();
+  },
+};
 </script>
 
 <template>
-  <AppHero></AppHero>
-  <AppTitle></AppTitle>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <div></div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+/* import file general.scss*/
+@use "./scss/general.scss";
 </style>
--- src/components/AppTitle.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h1>Vite-Boolfolio</h1>
-</template>
-<script></script>
-<style></style>
