9eb4788af9b4f020995982b4d77308547042a887
sisdream <147060423+sisdream@users.noreply.github.com>
2024-04-12 17:31:26 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+</script>
+
+<template>
+  <AppHero></AppHero>
+  <AppTitle></AppTitle>
+</template>
+
+<style scoped>
+</style>
