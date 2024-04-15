4e236bace01063e0d6a91680fcddbcf52fda6be0
sisdream <sissipascale2@gmail.com>
2024-04-15 16:54:59 +0200

    fix:aggiusti app e project

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import ProjectCard from "./components/ProjectCard.vue";
 export default {
   data() {
     return {
-      title: "Vite Boolfolio!",
+      title: "Hello world!",
       projects: [],
     };
   },
@@ -14,20 +14,18 @@ export default {
   components: {
     ProjectCard,
   },
-  created() {
-    this.fecthProjects();
-  },
 
   methods: {
     fecthProjects() {
-      axios.get(`http://127.0.0.1:8000/api/projects`)
-        .then((res) => {
-          console.log('>>> response fetch projects:', res.data)
-          this.projects = res.data.data;
-          console.log(this.projects)
-        })
-        .catch((err) => console.error('>>> errore fetch projects:', err));
+      axios.get(`http://127.0.0.1:8000/api/projects`).then((res) => {
+        console.log(res.data.data);
+        this.projects = res.data.data;
+      });
     },
+  },
+
+  mounted() {
+    this.fecthProjects();
   },
 };
 </script>
@@ -35,16 +33,12 @@ export default {
 <template>
   <div class="container">
     <h1>{{ title }}</h1>
-    <div v-if="projects.length > 0" class="row g-3">
+    <div class="row g-3">
       <project-card
         v-for="project in projects"
         :project="project"
       ></project-card>
     </div>
-
-   
-
-    <div v-else>Nulla da mostrare</div>
   </div>
 </template>
 
